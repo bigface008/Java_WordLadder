28946ed109f51d1be803cff19f9a64d7a92486a0
bigface008 <bigface00@sjtu.edu.cn>
2018-03-07 12:29:30 +0800

    Working.

--- WordLadder.java
+++ WordLadder.java
@@ -3,7 +3,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.file.Paths;
 import java.util.*;
 
 /**
@@ -16,11 +15,14 @@ public class WordLadder {
         if (args.length != 3) {
             System.out.println("Args are not enough.");
             System.exit(1);
+        } else if (args[1].length() != args[2].length()) {
+            System.out.println("The words must have the same length.");
+            System.exit(1);
         }
 
         try {
             // Build the dictionary.
-            Set<String> dictionary = dictionaryBuilder(args[0]);
+            Set<String> dictionary = dictionaryBuilder(args[0], args[1].length());
 
             // Try to find the shortest path.
             ladder(args[1], args[2], dictionary);
@@ -32,33 +34,72 @@ public class WordLadder {
         }
     }
 
-    static Set<String> dictionaryBuilder(String file_name) throws IOException {
+    static Set<String> dictionaryBuilder(String file_name, int len) throws IOException {
         File dic = new File(file_name);
         FileInputStream fin = new FileInputStream(dic);
         BufferedReader br = new BufferedReader(new InputStreamReader(fin));
 
         String line = null;
         Set<String> dictionary = new HashSet<>();
-        while ((line = br.readLine()) != null) {
-            dictionary.add(line);
-        }
+        while ((line = br.readLine()) != null)
+            if (line.length() == len)
+                dictionary.add(line);
 
         br.close();
         return dictionary;
     }
 
     static void ladder(String word_1, String word_2, Set<String> dictionary) {
-        if (word_1.length() != word_2.length()) {
-            System.out.println("The words must have the same length.");
-            System.exit(1);
+        Queue<String> path = new LinkedList<>();
+        Map<String, String> map_of_path = new HashMap<>();
+        path.add(word_1);
+        while (!path.isEmpty()) {
+            String temp = path.poll();
+            dictionary.remove(temp);
+            ArrayList<String> neighbors = findNeighbors(temp, dictionary);
+            for (String n : neighbors) {
+                map_of_path.put(n, temp);
+                if (n.equals(word_2)) {
+                    System.out.println("Paths: " + findPath(map_of_path, word_1, word_2));
+                    System.exit(0);
+                }
+                path.add(n);
+            }
+        }
+        System.out.println("No such path between " + word_1 + " and " + word_2 + ".");
+    }
+
+    static ArrayList<String> findNeighbors(String word, Set<String> dictionary) {
+        ArrayList<String> neighbors = new ArrayList<>();
+        for (String str : dictionary) {
+            int len = word.length();
+            int check = 0;
+            for (int i = 0; i < len; i++) {
+                if (word.charAt(i) != str.charAt(i))
+                    check++;
+                if (check > 1)
+                    break;
+            }
+            if (check == 1)
+                neighbors.add(str);
+        }
+        return neighbors;
+    }
+
+    static String findPath(Map<String, String> map, String word_1, String word_2) {
+        ArrayList<String> path = new ArrayList<>();
+        path.add(word_2);
+        String temp = map.get(word_2);
+        while (!temp.equals(word_1)) {
+            path.add(temp);
+            temp = map.get(temp);
         }
 
-        Set<String> small_dictionary = new HashSet<>();
-        for (String str : dictionary)
-            if (str.length() == word_1.length())
-                small_dictionary.add(str);
-        
-        int size_of_dictionary = small_dictionary.size();
-        List<String> path = new LinkedList<>();
+        String result = "";
+        for (String word : path) {
+            result += (word + " ");
+        }
+        result += (word_1 + " ");
+        return result;
     }
 }
